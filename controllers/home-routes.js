5688cea14feace90954fd462bfa5d1be9cf4b70b
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2021-11-07 15:56:02 -0800

    updated connection.js, and controllers fodlder v1

---
+++ controllers/home-routes.js
@@ -0,0 +1,113 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+
+router.get('/', (req, res) => {
+  // console log session information
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'contents',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn
+       });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  } else {
+    res.render('login');
+  }
+  
+});
+
+router.get('/signUp', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  } else {
+    res.render('signUp');
+  }
+  
+});
+
+
+// logic for single-post handlebar
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'contents',
+      'created_at'
+
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data be visible as an object
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', { 
+        post,
+        loggedIn: req.session.loggedIn
+     });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,23 @@
- test 
+const router = require('express').Router();
+
+const homeRoutes = require('./home-routes.js');
+
+const apiRoutes = require('./api');
+
+const dashboardRoutes = require('./dashboard-routes.js');
+
+
+
+// routing paths
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+// 404 Not Found HTTP request was not found
+router.use((req, res) => {
+    res.status(404).end();
+  });
+
+// Export the middle router for usage @ router incoming 
+// from server node.js express session
+module.exports = router;
